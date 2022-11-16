57be48f8d104ead4d675274cf467e99b630456ab
Alessandro Minisini <aleminipublic@gmail.com>
2022-11-16 17:04:17 +0100

    added lab ex

--- nostars/es9.cpp
+++ nostars/es9.cpp
@@ -5,23 +5,22 @@
 using namespace std;
 
 int main(){
-vector<int> v;
-int x;
-cin>>v>>x;
-int n=v.size();
-if(n!=0){
-  int j=0;
-  int s=v[0];
-  for(int i=1;i<n;i++){
-    s+=v[i];
-    while(j<=min(i,n-1)&&(s>x||v[j]==0))s-=v[j++];
-    if(j==n)break;
-    if(s==x){
-      cout<<j<<" "<<i<<endl;
-      return 0;
+  vector<int> v;
+  int x;
+  cin>>v>>x;
+  int n=v.size();
+  if(n!=0){
+    int j=0;
+    int s=v[0];
+    for(int i=1;i<n;i++){
+      s+=v[i];
+      while(j<=min(i,n-1)&&(s>x||v[j]==0))s-=v[j++];
+      if(j==n)break;
+      if(s==x){
+        cout<<j<<" "<<i<<endl;
+        return 0;
+      }
     }
   }
-}
-cout<<"-1 -1"<<endl;
-
+  cout<<"-1 -1"<<endl;
 }
